3bec3316c53aec601117a35a8859617a3e9320b4
COMPU-S040\daw2 <joacotto05-clase>
2026-09-21 20:24:01 +0200

    ejercicio php (21/09)(no terminado aún)

---
+++ 1ERA EV/DWES (BACK)/php2Actividad/a3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    // actividad 1
+        $arrayProductos= array("Leche", "Huevos", "Pollo", "Soja Texturizada", "Ben");
+            foreach ($arrayProductos as $indice => $producto){
+                echo $indice . " , " . $producto
+            }
+    ?>
+
+    <?php
+    // actividad 2
+        $arrayAsociativo = array(
+            array("Leche", 1.50€, 50),
+            array("Huevos", 3.50€, 50),
+            array("Pollo", 5€, 25),
+            array("Soja texturizada", 4.25€, 50)
+        );
+
+        foreach($arrayAsociativo as $clave){
+            echo $clave . "  " . array
+        }
+    ?>
+
+    <?php
+    // actividad 3
+        $arrayAsociativo = array("Leche", 1.50€, 50);
+        
+    ?>
+
+    <?php
+    // actividad 4
+        
+    ?>
+</body>
+</html>
