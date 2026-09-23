6168aec042e923c1ca965b71ab9a1f1e58285e9b
COMPU-S040\daw2 <joacotto05-clase>
2026-09-23 19:53:05 +0200

    actividad 3 php (terminado 23/09)

--- 1ERA EV/DWES (BACK)/php2Actividad/a3.php
+++ 1ERA EV/DWES (BACK)/php2Actividad/a3.php
@@ -11,33 +11,63 @@
     // actividad 1
         $arrayProductos= array("Leche", "Huevos", "Pollo", "Soja Texturizada", "Ben");
             foreach ($arrayProductos as $indice => $producto){
-                echo $indice . " , " . $producto
+                echo ($indice + 1) . " , " . $producto;
             }
     ?>
 
     <?php
     // actividad 2
-        $arrayAsociativo = array(
-            array("Leche", 1.50€, 50),
-            array("Huevos", 3.50€, 50),
-            array("Pollo", 5€, 25),
-            array("Soja texturizada", 4.25€, 50)
+        $producto = array(
+            "nombre" => "Leche",
+            "precio" => 1.50,
+            "stock" => 50
         );
 
-        foreach($arrayAsociativo as $clave){
-            echo $clave . "  " . array
-        }
+        echo "Nombre: " . $producto["nombre"] . "<br>";
+        echo "Precio: " . $producto["precio"] . " €<br>";
+        echo "Stock: " . $producto["stock"] . " unidades<br>";
     ?>
 
     <?php
     // actividad 3
-        $arrayAsociativo = array("Leche", 1.50€, 50);
-        
+        $producto1 = array(
+            "nombre" => "Leche",
+            "precio" => 1.50,
+            "stock" => 50
+        );
+
+        $producto2 = array(
+            "nombre" => "Huevos",
+            "precio" => 3.50,
+            "stock" => 50
+        );
+
+        $total = ($producto1["precio"] * $producto1["stock"]) + ($producto2["precio"] * $producto2["stock"]);
+
+        echo "Precio total de la compra: " . $total . " €";
     ?>
 
     <?php
     // actividad 4
-        
+        $productos = array(
+            array("nombre" => "Leche", "precio" => 1.50, "categoria" => "Lacteos"),
+            array("nombre" => "Queso", "precio" => 2.20, "categoria" => "Lacteos"),
+            array("nombre" => "Pan", "precio" => 1.10, "categoria" => "Panaderia"),
+            array("nombre" => "Croissant", "precio" => 1.80, "categoria" => "Panaderia")
+        );
+
+        $categoriaBuscada = "Lacteos";
+        $total = 0;
+
+        foreach ($productos as $producto) {
+            if ($producto["categoria"] == $categoriaBuscada) {
+                echo $producto["nombre"] . " - " . $producto["precio"] . " €<br>";
+                $total += $producto["precio"];
+            }
+        }
+
+        echo "Total de la categoría " . $categoriaBuscada . ": " . $total . " €";
     ?>
+
 </body>
 </html>
